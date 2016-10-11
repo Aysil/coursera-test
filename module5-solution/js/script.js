e25f1cbc8442e7a6f65ea7ec0c0b436e87716db2
Leonidas_eng <Leonidas_eng>
2016-10-11 12:47:46 +0300

    mod5

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -93,7 +93,7 @@ $ajaxUtils.sendGetRequest(
 // Builds HTML for the home page based on categories array
 // returned from the server.
 function buildAndShowHomeHTML (categories) {
-  console.log(homeHtmlUrl);
+  console.log(categories[0]);
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
