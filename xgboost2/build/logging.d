8a448ed21ea211272a58354f8c7e6d28e93c12c5
Aysil <teka-kor@yandex.ru>
2017-07-12 21:00:52 +0300

    big J

---
+++ xgboost2/build/logging.d
@@ -0,0 +1,19 @@
+build/logging.o: src/logging.cc include/xgboost/logging.h \
+ dmlc-core/include/dmlc/logging.h dmlc-core/include/dmlc/./base.h \
+ include/xgboost/./base.h dmlc-core/include/dmlc/base.h \
+ dmlc-core/include/dmlc/omp.h src/./common/sync.h \
+ rabit/include/rabit/rabit.h rabit/include/rabit/./internal/engine.h \
+ rabit/include/rabit/./internal/../serializable.h \
+ rabit/include/rabit/./internal/.././internal/utils.h \
+ C:/Users/Teka/xgboost/rabit/include/dmlc/io.h \
+ C:/Users/Teka/xgboost/rabit/include/dmlc/logging.h \
+ C:/Users/Teka/xgboost/rabit/include/dmlc/serializer.h \
+ C:/Users/Teka/xgboost/rabit/include/dmlc/base.h \
+ C:/Users/Teka/xgboost/rabit/include/dmlc/io.h \
+ C:/Users/Teka/xgboost/rabit/include/dmlc/type_traits.h \
+ rabit/include/rabit/./internal/rabit-inl.h \
+ rabit/include/rabit/./internal/./io.h \
+ rabit/include/rabit/./internal/././utils.h \
+ rabit/include/rabit/./internal/./../serializable.h \
+ rabit/include/rabit/./internal/./utils.h \
+ rabit/include/rabit/./internal/../rabit.h
